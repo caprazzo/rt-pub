77deed7dc6c28e5ecb9c23a05021af96695be015
Matteo Caprari <matteo.caprari@gmail.com>
2010-02-11 17:43:17 +0000

    initial likestore implementation

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ visited_users = []
 queue = ['04542763409539038815']
 triples = set([])
 
+def store(subj, pred, obj):
+	f = open('store.txt', 'a')
+	f.write(json.dumps((subj, pred, obj)) + '\n')
+	f.close()
+
 def shared_items(user):
 
 def drill_user(user):
 	for item in shared_items(user):
-		store(user, 'shares', item.id)
+		store(item.id, 'dc:title', item.title) 
+		store(user, 'cu:shares', item.id)
 		if item.id not in visited_items:
 			visited_items.add(item.id)
 			for liker in gr.item_likers(item.id)
-				store(liker, 'likes', item.id)
+				store(liker, 'cu:likes', item.id)
 				queue.append(liker)
 					
 def run():
